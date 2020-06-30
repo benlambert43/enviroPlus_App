4aeee0b0f908a41763416a670adaecc85b532504
benlambert43 <benlambert43@gmail.com>
2020-06-30 12:31:57 -0600

    Home Screen

--- hiHome/Components/Home.js
+++ hiHome/Components/Home.js
@@ -1,115 +1,57 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState } from "react";
+import HomeContent from "./HomeContent";
 import {
-  StyleSheet,
-  Text,
   SafeAreaView,
+  StyleSheet,
   ScrollView,
-  View,
-  ActivityIndicator,
-  Image,
+  RefreshControl,
 } from "react-native";
 import Constants from "expo-constants";
-import { DataContext } from "./DataContext";
-import HomeCard from "./HomeCard";
-import HomeCardMap from "./HomeCardMap";
 
-const timeOfDay = () => {
-  const hour = new Date().getHours();
-  if (hour <= 11 && hour >= 3) {
-    return "Morning";
-  } else if (hour <= 17 && hour > 11) {
-    return "Afternoon";
-  } else if (hour < 3 || hour > 17) {
-    return "Evening";
-  }
-};
+const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState({ outdoor: {}, indoor: {} });
 
-export default function Home() {
-  const [inFlight, setInFlight] = useState(false);
-  const apiData = useContext(DataContext);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    const getMoviesFromApiAsync = async () => {
+      try {
+        let outdoorResponse = await fetch(
+          "http://api.openweathermap.org/data/2.5/weather?id=5429032&appid=b6bb5c7d10fde13ec854ea2b9dd19c69&units=imperial"
+        );
+        let indoorResponse = await fetch(
+          "http://10.0.0.91:4000/envirodata/pullAll"
+        );
+        let outdoorJSON = await outdoorResponse.json();
+        let indoorJSON = await indoorResponse.json();
 
+        setData({ outdoor: outdoorJSON, indoor: indoorJSON });
+        setRefreshing(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getMoviesFromApiAsync();
+  }, [refreshing]);
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        {apiData.outdoor.main ? (
-          <>
-            <>
-              <View
-                style={{
-                  flexDirection: "row",
-                  height: 100,
-                  minHeight: 100,
-                  maxHeight: 100,
-                  padding: 10,
-                  marginTop: 10,
-                  marginBottom: 10,
-                  alignSelf: "center",
-                  backgroundColor: "white",
-                  borderRadius: 4,
-                  height: "15%",
-                  shadowColor: "black",
-                  width: "95%",
-                  shadowOffset: {
-                    width: 5,
-                    height: 5,
-                  },
-                  shadowOpacity: 0.1,
-                  shadowRadius: 1,
-                }}
-              >
-                <Text
-                  style={{
-                    alignSelf: "center",
-                    fontSize: 28,
-                    fontFamily: "System",
-                    paddingLeft: 10,
-                  }}
-                >
-                  Good {timeOfDay()}.
-                </Text>
-                <View
-                  style={{
-                    alignSelf: "center",
-                  }}
-                >
-                  <Image
-                    style={{
-                      width: 50,
-                      height: 50,
-                    }}
-                    source={{
-                      uri: `http://openweathermap.org/img/wn/${apiData.outdoor.weather[0].icon}@2x.png`,
-                    }}
-                  />
-                </View>
-              </View>
-            </>
-            <>
-              <HomeCardMap outdoor={apiData.outdoor} indoor={apiData.indoor} />
-            </>
-          </>
-        ) : (
-          <View style={{ flex: 1, justifyContent: "center", paddingTop: 300 }}>
-            <ActivityIndicator />
-          </View>
-        )}
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <HomeContent data={data} />
       </ScrollView>
     </SafeAreaView>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
   },
-  scrollView: {
-    marginHorizontal: 20,
-  },
-  text: {
-    fontSize: 18,
-    fontFamily: "System",
-    padding: 5,
-    textAlign: "center",
-  },
+  scrollView: {},
 });
+
+export default Home;
--- hiHome/Components/HomeCardMap.js
+++ hiHome/Components/HomeCardMap.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomeCard from "./HomeCard";
+import { View } from "react-native";
 
 const HomeCardMap = (props) => {
   const cardData = [
@@ -11,25 +12,76 @@ const HomeCardMap = (props) => {
         props.outdoor.main.feels_like
       )}°F with a wind speed of ${props.outdoor.wind.speed} from ${
         props.outdoor.wind.deg
-      }°. The outside pressure is ${
-        props.outdoor.main.pressure
-      } hPa and the humidity is ${props.outdoor.main.humidity}%`,
+      }°. The outside humidity is ${
+        props.outdoor.main.humidity
+      }% and the pressure is ${props.outdoor.main.pressure} hPa.`,
       icon: "ios-pin",
     },
     {
       key: 2,
-      text: `Indoor Conditions\n\n The current indoor temperature is ${parseInt(
+      text: `Indoor Conditions\n\nThe current indoor temperature is ${parseInt(
         (props.indoor[props.indoor.length - 1].currentAdjustedTemp * 9) / 5 + 32
-      )}`,
+      )}°F.\nThe indoor humidity is ${parseInt(
+        props.indoor[props.indoor.length - 1].currentHumidity
+      )}%, and the indoor pressure is ${parseInt(
+        props.indoor[props.indoor.length - 1].currentPressure
+      )} hPa.`,
       icon: "ios-home",
+    },
+    {
+      key: 3,
+      text: `Gas Sensing\n\nThe current reducing (CO) gas reading is ${
+        Math.round(
+          (parseFloat(props.indoor[props.indoor.length - 1].currentco2) +
+            Number.EPSILON) *
+            100
+        ) / 10000
+      }ppm, oxidising (NO2) gas reading is ${
+        Math.round(
+          (parseFloat(props.indoor[props.indoor.length - 1].currentno2) +
+            Number.EPSILON) *
+            100
+        ) / 10000
+      }ppb, and the NH3 gas reading is ${
+        Math.round(
+          (parseFloat(props.indoor[props.indoor.length - 1].currentnh3) +
+            Number.EPSILON) *
+            100
+        ) / 10000
+      }ppm`,
+      icon: "ios-cloudy",
+    },
+    {
+      key: 4,
+      text: `Light Sensing\n\nThe light sensor is reporting ${
+        Math.round(
+          (parseFloat(props.indoor[props.indoor.length - 1].currentLight) +
+            Number.EPSILON) *
+            100
+        ) / 100
+      } Lux`,
+      icon: "ios-flashlight",
+    },
+    {
+      key: 5,
+      text: `Hardware Information\n\nThe current CPU temperature is ${
+        Math.round(
+          (parseFloat(props.indoor[props.indoor.length - 1].currentCpuTemp) +
+            Number.EPSILON) *
+            100
+        ) / 100
+      }°C\n\nThe last server sync was at ${
+        props.indoor[props.indoor.length - 1].currentTime
+      }`,
+      icon: "ios-information-circle",
     },
   ];
   return (
-    <>
+    <View style={{ paddingBottom: 40 }}>
       {cardData.map((item) => (
         <HomeCard item={item} key={item.key} />
       ))}
-    </>
+    </View>
   );
 };
 
